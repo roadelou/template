2dddd9bb960e94935e58c4dbd7790a3b43eead19
roadelou <tableau.roche@gmail.com>
2021-04-17 21:54:56 +0200

    Updated license prompt on command line.

--- src/main.c
+++ src/main.c
@@ -195,10 +195,20 @@ int print_help(void) {
 int print_license(void) {
     return printf(
         "%s\n",
-        "template Copyright (C) 2020 roadelou\n"
-        "This program comes with ABSOLUTELY NO WARRANTY.\n"
-        "This is free software, and you are welcome to redistribute it\n"
-        "under certain conditions.\n"
+        "template is a small C executable to build templated files from the\n"
+        "command line. Copyright (C) 2021  roadelou\n"
         "\n"
-        "See the GPL-3.0 on https://www.gnu.org/licenses/gpl-3.0.en.html");
+        "This program is free software: you can redistribute it and/or modify\n"
+        "it under the terms of the GNU General Public License as published by\n"
+        "the Free Software Foundation, either version 3 of the License, or\n"
+        "(at your option) any later version.\n"
+        "\n"
+        "This program is distributed in the hope that it will be useful,\n"
+        "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+        "GNU General Public License for more details.\n"
+        "\n"
+        "You should have received a copy of the GNU General Public License\n"
+        "along with this program.  If not, see "
+        "<https://www.gnu.org/licenses/>.");
 }
