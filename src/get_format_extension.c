roadelou/template

commit e002eb41701777775d7f020e14889124fb339031

Corrected issue #1
The problem arised when the first character of the string was a dot,
which led to some value not being incremented correctly since index 0
was handled with a separate case.

src/get_format_extension.c
@@ -342,9 +342,10 @@ static size_t next_extension_part_backwards(const char *path, size_t *cursor) {
      - next_cursor = 0;
      - next_cursor >= 0 and next_cursor points to a '.';
      - next_cursor >= 0 and next_cursor points to a '/';
-    The right value to return is next_cursor + (next_cursor > 0);
+    In either of the last two cases, we should increment the returned cursor.
     */
-    return next_cursor + (next_cursor > 0);
+    return next_cursor + (*(path + next_cursor) == '.') ||
+           (*(path + next_cursor) == '/');
 }
 
 static size_t next_extension_part_forwards(const char *path, size_t *cursor) {

commit 8f97688a5390263964f3e6318d944d89385dcdae

Solved operator priority issue in new matching alg
The bug had been introduced by e002eb4 while solving #1

src/get_format_extension.c
@@ -344,8 +344,8 @@ static size_t next_extension_part_backwards(const char *path, size_t *cursor) {
      - next_cursor >= 0 and next_cursor points to a '/';
     In either of the last two cases, we should increment the returned cursor.
     */
-    return next_cursor + (*(path + next_cursor) == '.') ||
-           (*(path + next_cursor) == '/');
+    return next_cursor +
+           ((*(path + next_cursor) == '.') || (*(path + next_cursor) == '/'));
 }
 
 static size_t next_extension_part_forwards(const char *path, size_t *cursor) {