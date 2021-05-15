7e098b47d2fec7dd63a437944e7752ce7d492044
roadelou <tableau.roche@gmail.com>
2021-05-15 17:37:16 +0200

    Rebased `format_extension` with List

--- src/format_extension.c
+++ src/format_extension.c
@@ -12,6 +12,9 @@
 /* The header containing the functions we are trying to implement. */
 #include <template.h>
 
+/* Used for realloc and free. */
+#include <stdlib.h>
+
 /********************************* PROTOYPES **********************************/
 
 /*
@@ -48,65 +51,79 @@ long file_size(FILE *file) {
     return returned_size;
 }
 
-char *format_extension(const char *extension) {
-    /* The HOME environment variable, used to find the template files. */
-    char *home_path = getenv("HOME");
-    /* The first thing to do is to create the name of the file where the
-     * template string should be stored. Because this is only usefull at the
-     * beginning, we will reuse the variable long later on to hold the size of
-     * the format file. This is why it is a long and not an int. */
-    long length = strlen(extension) + strlen(home_path);
-    /* We allocate on the stack an array big enough to fit the name of the file
-     * we are going to try to read. This will be reused later on to hold the
-     * format string we will return. */
-    char format_path[length + 37];
+char *format_extension(const struct List *list, const char *extension) {
+    /* This buffer is used to temporarily store the format paths during the
+     * search. It is set to NULL initialy for realloc. */
+    char *format_path = NULL;
+    /* This variable will hold the number of characters for the format path, it
+     * is used for allocation. */
+    size_t format_path_length;
     /* This is the buffer that we will dynamically allocate to hold the format
      * string. */
     char *format_string;
+    /* The length of the entire template string, used for memory allocation. */
+    size_t format_string_length;
     /* The handle to the format file, once we will have opened it. */
     FILE *format_file;
-    /* First location tried: in ~/.config/roadelou_template. */
-    /* We create the name of the format file. This should never fail because our
-     * buffer is big enough to hold the path. */
-    snprintf((char *)format_path, sizeof(format_path),
-             "%s/.config/roadelou_template/%s.template", home_path, extension);
-    /* We try to open the file. */
-    format_file = fopen((char *)format_path, "r");
-    /* We fail if the file could not be opened. */
-    if (format_file == NULL) {
-        /* Second location tried: in /etc/roadelou_template. */
-        /* Note that /etc is always shorter than /home/..., hence this will fit
-         * in our buffer. */
-        snprintf((char *)format_path, sizeof(format_path),
-                 "/etc/roadelou_template/%s.template", extension);
 
+    /* We go through all the provided paths in order until we find one where
+     * the extension matches what we have been given. */
+    for (size_t i = 0; i < list->length; i++) {
+        /* We compute the size of the format path. 9 is for ".template" and 1 is
+         * for the NULL byte. */
+        format_path_length =
+            (strlen(*(list->strings + i)) + strlen(extension) + 9 + 1) *
+            sizeof(char);
+        /* We resize our format_path to hold the path to the new file. For now
+         * we assume that this never fails. */
+        format_path = realloc(format_path, format_path_length);
+        /* We create the name of the format file. This should never fail because
+         * our buffer is big enough to hold the path. */
+        snprintf(format_path, format_path_length, "%s/%s.template",
+                 *(list->strings + i), extension);
         /* We try to open the file. */
         format_file = fopen((char *)format_path, "r");
-        /* We fail if the file could not be opened. */
         if (format_file == NULL) {
-            /* If this also fails, then we return NULL. */
-            return NULL;
+            /* The file did not exist, we move on to the next search path. */
+            continue;
+        } else {
+            /* We found the template file to use, there is no need to search
+             * any further. */
+            break;
         }
     }
+
+    /* If we found no valid format file in any of the directories of the
+     * list, we simply free the memory used and return NULL. */
+    if (format_file == NULL) {
+        free(format_path);
+        return NULL;
+    }
+
     /* Else we grab the size of the file. Notice that we reuse the variable
      * length from the beginning. */
-    length = file_size(format_file);
+    format_string_length = file_size(format_file);
     /* We allocate some memory dynamically to hold the big format string. */
-    format_string = malloc(length + 1);
-    /* We have enough room in out buffer, so we simply copy the format string
-     * from our file. We check for errors at the same time. Because we have a
-     * single condition here, the call to fread will always take place. */
-    if (fread(format_string, 1, length, format_file) < length) {
+    format_string = malloc(format_string_length + 1);
+    /* We have enough room in out buffer, so we simply copy the format
+     * string from our file. We check for errors at the same time. Because
+     * we have a single condition here, the call to fread will always take
+     * place. */
+    if (fread(format_string, 1, format_string_length, format_file) <
+        format_string_length) {
         fclose(format_file);
         free(format_string);
+        free(format_path);
         /* We failed for some mysterious reason. */
         return NULL;
     }
-    /* We have successfully gotten our format string, we just have to close the
-     * file and return our result. */
+    /* We have successfully gotten our format string, we just have to close
+     * the file and return our result. */
     fclose(format_file);
+    /* We free the memory associated with the format path. */
+    free(format_path);
     /* We add a null termination byte. */
-    *(format_string + length) = '\0';
+    *(format_string + format_string_length) = '\0';
     return format_string;
 }
 
