77cb46b920d7b5bf5fae1d9a3f7179a63f678c38
roadelou <tableau.roche@gmail.com>
2021-11-14 20:51:13 +0100

    Added unifiying header for testing functions
    Note that the testing functions aren't exported by default when
    including the template.h header.

---
+++ include/template/test.h
@@ -0,0 +1,43 @@
+/* Include once header guard */
+#ifndef TEMPLATE_TEST_INCLUDED
+#define TEMPLATE_TEST_INCLUDED
+
+/********************************** METADATA **********************************/
+
+/*
+ * Contributors: roadelou
+ * Contacts:
+ * Creation Date: 2021-11-14
+ * Language: C Header
+ * Compiler: cc (GCC) 11.2.1 20210728 (Red Hat 11.2.1-1)
+ */
+
+/********************************** INCLUDES **********************************/
+
+/* Included the test declarations. */
+#include <template/test/test_header.h>
+#include <template/test/test_integer.h>
+#include <template/test/test_string.h>
+
+/*********************************** MACROS ***********************************/
+
+/* The macros definitions for your header go here */
+
+/********************************** STRUCTS ***********************************/
+
+/* The enums of your header go here */
+
+/* The structs of your header go here */
+
+/* The unions of your header go here */
+
+/* The typedefs of your header go here */
+
+/********************************* PROTOTYPES *********************************/
+
+/* The prototypes for your header go here */
+
+/* End of include once header guard */
+#endif
+
+/************************************ EOF *************************************/
